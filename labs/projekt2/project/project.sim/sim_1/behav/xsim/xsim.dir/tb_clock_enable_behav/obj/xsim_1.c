1c89caff8c0f5655ae619a87e79bd6550c29921d
marekcrn <marekcrn@seznam.cz>
2022-04-29 18:41:14 +0200

    projekt

--- labs/projekt2/project/project.sim/sim_1/behav/xsim/xsim.dir/tb_clock_enable_behav/obj/xsim_1.c
+++ labs/projekt2/project/project.sim/sim_1/behav/xsim/xsim.dir/tb_clock_enable_behav/obj/xsim_1.c
@@ -66,8 +66,8 @@ const int NumRelocateId= 6;
 void relocate(char *dp)
 {
 	iki_relocate(dp, "xsim.dir/tb_clock_enable_behav/xsim.reloc",  (void **)funcTab, 6);
-	iki_vhdl_file_variable_register(dp + 2624);
-	iki_vhdl_file_variable_register(dp + 2680);
+	iki_vhdl_file_variable_register(dp + 2672);
+	iki_vhdl_file_variable_register(dp + 2728);
 
 
 	/*Populate the transaction function pointer field in the whole net structure */
